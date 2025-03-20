34c6db26ff47cbf8029d0ca5ee9c9cf665ce4a84
blyator <dmnbilly@gmail.com>
2025-03-20 17:02:36 +0300

    Running code

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((data) => {
+      renderBooks(data);
+      return data;
+    });
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+function renderBooks(data) {
+  const main = document.querySelector("main");
+
+  data.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener("DOMContentLoaded", fetchBooks);
